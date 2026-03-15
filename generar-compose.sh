c7e6996171766499f839f3b451d907fd61c622b8
SJAnton <santiagogranate13@gmail.com>
2026-03-15 19:37:29 -0300

    feat: generate Docker Compose file with n clients via shell script

---
+++ generar-compose.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+filename=$1
+client_amount=$2
+
+> "$filename"
+
+echo "name: tp0" >> "$filename"
+echo "services:" >> "$filename"
+
+cat << EOF >> "$filename"
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+EOF
+
+echo >> "$filename"
+
+for i in $(seq 1 $client_amount)
+do
+
+cat << EOF >> "$filename"
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+EOF
+
+echo >> "$filename"
+done
+
+cat << EOF >> "$filename"
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+EOF
